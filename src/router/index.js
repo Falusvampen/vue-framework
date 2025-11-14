f273faec0ca7e89b0e8bc3112e384dbacdca311f
sannaglarsson <sannaglarsson@gmail.com>
2025-11-14 12:03:13 +0100

    testing, create a test view

--- src/router/index.js
+++ src/router/index.js
@@ -23,6 +23,11 @@ const router = createRouter({
       // which is lazy-loaded when the route is visited.
       component: () => import('../views/AboutView.vue'),
     },
+    {
+      path: '/test',
+      name: 'test',
+      component: () => import('../views/TestView.vue'),
+    }
   ],
 })
 
---
+++ src/views/TestView.vue
@@ -0,0 +1,9 @@
+<script setup>
+import titleAndDescription from '@/components/titleAndDescription.vue'
+</script>
+
+<template>
+  <main>
+    <titleAndDescription />
+  </main>
+</template>
