efeccfb8216d564be6d2a12dad33680ad1a1ab96
sannaglarsson <sannaglarsson@gmail.com>
2025-11-14 12:03:13 +0100

    testing, create a test view

--- src/components/titleAndDescription.vue
+++ src/components/titleAndDescription.vue
@@ -1,9 +1,19 @@
+<script>
+export default {
+  props: {
+    title: String,
+    description: String,
+  }
+}
+</script>
+
 <template>
-    <nav>
-        <ol>
-            <li>
-                
-            </li>
-        </ol>
-    </nav>
+    <div>
+        <h1>Recept-hemsida</h1>
+        <p>Välkommen till denna unika receptsida för dig som gillar mat från hela världen! </p>
+    </div>
 </template>
+
+<style scoped>
+    
+</style>
