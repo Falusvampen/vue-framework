a3d05e0cddeba93a267d7a3951d5d0e6f3501f96
sannaglarsson <sannaglarsson@gmail.com>
2025-11-14 12:09:58 +0100

    create branch for component and basic build

---
+++ src/components/CategoryComponent.vue
@@ -0,0 +1,21 @@
+<script>
+export default {
+    props: {
+        catogory: String,
+
+    }
+}
+</script>
+
+<template>
+    <nav>
+        <h2>Kategorier</h2>
+        <ol>
+            <li> Ex {{ category }}</li>
+            <li> Ex {{ category }}</li>
+            <li> Ex {{ category }}</li>
+            <li> Ex {{ category }}</li>
+        </ol>
+    </nav>
+    
+</template>
