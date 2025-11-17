5ee0348b8e267fedbaff85f87ddf76f21a285bc6
sannaglarsson <sannaglarsson@gmail.com>
2025-11-17 15:53:08 +0100

    basic styling

--- src/components/CategoryComponent.vue
+++ src/components/CategoryComponent.vue
@@ -2,7 +2,6 @@
 export default {
     props: {
         catogory: String,
-
     }
 }
 </script>
@@ -17,5 +16,27 @@ export default {
             <li> Ex {{ category }}</li>
         </ol>
     </nav>
-    
+
 </template>
+
+<style scoped>
+  nav {
+    display: flex;
+    flex-direction: column;
+    background-color: rgb(189, 187, 187);
+    color: black;
+    padding: 10px;
+  }
+
+  h2 {
+    font-weight: bold;
+  }
+
+  ol {
+    list-style-type: none;
+  }
+
+  li:hover {
+    text-shadow: 2px 2px 4px rgba(0,0,0,0.4);
+  }
+</style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -3,6 +3,7 @@ import AxelComponent from '@/components/AxelComponent.vue'
 import KimComponent from '@/components/KimComponent.vue'
 import MohammedComponent from '@/components/MohammedComponent.vue'
 import SannaComponent from '@/components/SannaComponent.vue'
+import CategoryComponent from '@/components/CategoryComponent.vue'
 </script>
 
 <template>
@@ -11,5 +12,6 @@ import SannaComponent from '@/components/SannaComponent.vue'
     <SannaComponent />
     <KimComponent />
     <MohammedComponent />
+    <CategoryComponent />
   </main>
 </template>
