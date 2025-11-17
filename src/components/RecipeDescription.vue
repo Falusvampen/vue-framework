de54547c3bb3bb7b3e2c8dc9b52d7e84589d0ef6
Rownok Alam <mohammed.alam@chasacademy.se>
2025-11-17 21:26:17 +0100

    recipe description template

---
+++ src/components/RecipeDescription.vue
@@ -0,0 +1,27 @@
+<script>
+</script>
+
+<template>
+    
+    <div class="recipe-card">
+    </div>
+
+    <div class="recipe-header">
+        <h1 class="recipe-title">
+            {{ recipe.title }}
+        </h1>
+        <p class="recipe-description">
+            {{ recipe.description }}
+        </p>
+
+    </div>
+
+    <div class="recipeimg">
+
+    <img src="C:\Users\rowno\Desktop\receptprojekt\image.png" alt="recipe.title" />
+    </div>
+</template>
+
+
+<style>
+</style>
