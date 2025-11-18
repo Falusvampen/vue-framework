0debfadbac3c830119eb97b8e0492e62240d943b
Falusvampen <102164648+Falusvampen@users.noreply.github.com>
2025-11-18 13:53:11 +0100

    Delete src/router/index.js

--- src/router/index.js
+++
@@ -1,9 +0,0 @@
-import { createRouter, createWebHistory } from 'vue-router'
-import getComponentsFromViewsFiles from './autoRouter'
-
-const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
-  routes: getComponentsFromViewsFiles(),
-})
-
-export default router
