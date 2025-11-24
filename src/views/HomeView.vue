9f995a82119e6838e1f297285ea11597766055bd
Kimppa06 <Kimkarhusaari@gmail.com>
2025-11-24 14:53:54 +0100

    Finished IngredienserComponent

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,15 +1,9 @@
 <script setup>
-import AxelComponent from '@/components/AxelComponent.vue'
-import KimComponent from '@/components/KimComponent.vue'
-import MohammedComponent from '@/components/MohammedComponent.vue'
-import SannaComponent from '@/components/SannaComponent.vue'
+
 </script>
 
 <template>
   <main>
-    <AxelComponent />
-    <SannaComponent />
-    <KimComponent />
-    <MohammedComponent />
+    
   </main>
 </template>
