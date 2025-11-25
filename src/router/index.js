0e0b0819b13359c9e222d15c4d06d3b0b5722df0
Kimppa06 <Kimkarhusaari@gmail.com>
2025-11-25 12:19:42 +0100

    Index.js fix

--- src/router/index.js
+++ src/router/index.js
@@ -1,24 +1,9 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
-import Ingredienser from '../components/Ingredienser.vue'
-
 import getComponentsFromViewsFiles from './autoRouter'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [
-    {
-      path: '/',
-      name: 'home',
-      component: HomeView,
-    },
-    {
-      path: '/ingredienser',
-      name: 'ingredienser',
-      component:Ingredienser,
-    }
-    
-  ]
+  routes: getComponentsFromViewsFiles(),
 })
 
 export default router
