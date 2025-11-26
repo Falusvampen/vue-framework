2153418c8f8963c275c725d7375784a6dc2c5680
Kimppa06 <Kimkarhusaari@gmail.com>
2025-11-26 15:07:43 +0100

    Searchbar edit (#35)

--- src/components/Searchbar.vue
+++ src/components/Searchbar.vue
@@ -11,23 +11,38 @@ export default {
 <template>
   <div class="searchbar">
     <input type="text" :placeholder="placeholder" class="search-input" />
+    <img src="/icon.png" alt="Search icon" class="search-icon" />
   </div>
 </template>
+
+
 
 <style scoped>
 .searchbar {
   display: flex;
-  justify-content: center;
+  justify-content: space-between; 
+  align-items: center;
   margin-top: 5%;
+  width: 30rem;
+  padding: 2px 10px;
+  border: 1px solid #ccc;
+  border-radius: 3vh;
+  background-color: white;
 }
 
 .search-input {
-  width: 30rem;
-  height: 2.3rem;
-  border-radius: 3vh;
-  border: 1px solid #ccc;
-  padding: 0 15px;
-  font-size: 1rem;
+  border: none;
   outline: none;
+  font-size: 1rem;
+  width: 100%;
+  background: transparent;
+}
+
+.search-icon {
+  width: 2rem;
+  height: 2em;
 }
 </style>
+
+
+
