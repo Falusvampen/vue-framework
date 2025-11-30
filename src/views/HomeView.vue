8f7087780ec96dd7a69b6c7aa5be8e7a50b9d2e5
Rownok Alam <mohammed.alam@chasacademy.se>
2025-11-30 19:33:17 +0100

    added responsiveness to recipe description component

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -3,16 +3,14 @@ import AxelComponent from '@/components/AxelComponent.vue'
 import KimComponent from '@/components/KimComponent.vue'
 import MohammedComponent from '@/components/MohammedComponent.vue'
 import SannaComponent from '@/components/SannaComponent.vue'
+import RecipeDescription from '@/components/RecipeDescription.vue'
 
 </script>
 
 <template>
   <main>
-    <AxelComponent />
-    <KimComponent />
-    <MohammedComponent />
-    <SannaComponent />
    
+    <RecipeDescription />
     
   </main>
 </template>
