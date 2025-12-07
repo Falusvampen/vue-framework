f46b63b8dd61ff17ebaf7f4be7084a0bde74a687
Kimppa06 <Kimkarhusaari@gmail.com>
2025-12-07 20:01:43 +0100

    Scrollbehavior fix (#79)

--- src/router/index.js
+++ src/router/index.js
@@ -4,6 +4,10 @@ import getComponentsFromViewsFiles from './autoRouter'
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: getComponentsFromViewsFiles(),
+  scrollBehavior() {
+    return { top: 0 }
+  }
+
 })
 
 export default router
