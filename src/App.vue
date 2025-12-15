5c5d389cc3eec0536241c5418af40770a0b14998
Falusvampen <102164648+Falusvampen@users.noreply.github.com>
2025-12-15 16:47:54 +0100

    fix: add GlobalErrorNotification component to App.vue for global error handling

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,11 @@
 <script setup>
 import { RouterView } from 'vue-router'
 import HeaderComponent from './components/HeaderComponent.vue'
+import GlobalErrorNotification from './components/GlobalErrorNotification.vue'
 </script>
 
 <template>
+  <GlobalErrorNotification />
   <HeaderComponent />
   <RouterView />
 </template>
